8965da68b456dc7b9d47f09226242d18b1018163
Rob Dickinson <robfromboulder@gmail.com>
2019-06-02 22:24:35 -0700

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='usagelogger',
-    version='0.1.0',
+    version='0.1.1',
     description='Logging usage of Python cloud apps.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
