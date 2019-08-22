f3f327ee23714cd22d6daf8071aec240073f70ab
Rob Dickinson <robfromboulder@gmail.com>
2019-08-22 00:29:49 -0600

    Update version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='usagelogger',
-    version='0.2.1',
+    version='0.2.2',
     description='Logging usage of Python-based services, with user privacy by design.',
     long_description="""Logging usage of Python-based services, with user privacy by design.
 
