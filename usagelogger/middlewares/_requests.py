45bd61ff2036263de3cbd37c02c0679f3c1925b4
Anyesh <sthacruz@gmail.com>
2021-02-18 19:05:13 +0545

    Fix interval

--- usagelogger/middlewares/_requests.py
+++ usagelogger/middlewares/_requests.py
@@ -8,12 +8,22 @@ from .requests_adapter import BaseMiddleware
 class ResurfaceLoggerMiddleware(BaseMiddleware):
     def __init__(self, url, rules, *args, **kwargs):
         self.logger = HttpLogger(url=url, rules=rules)
+        self.start_time = 0
+
+    def before_build_response(self, req, resp):
+        """Called before `HTTPAdapter::build_response`. Optionally modify the
+        returned `PreparedRequest` and `HTTPResponse` objects.
+        :param req: The `PreparedRequest` used to generate the response.
+        :param resp: The urllib3 response object.
+        :returns: Tuple of potentially modified (req, resp)
+        """
+
+        self.start_time = time.time()
+        return req, resp
 
     def after_build_response(self, req, resp, response):
 
-        start_time = time.time()
-        interval = str((time.time() - start_time) * 1000)
-
+        interval = str((time.time() - self.start_time) * 1000)
         HttpMessage.send(
             self.logger, request=req, response=response, interval=interval,
         )
