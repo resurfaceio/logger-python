bc303beca8e13d1eb753c9c8b7c1f7a5cce8c0b5
Anyesh <sthacruz@gmail.com>
2021-02-23 15:45:59 +0545

    Add flask middleware

--- usagelogger/middlewares/__init__.py
+++ usagelogger/middlewares/__init__.py
@@ -1,5 +1,11 @@
 from ._django import HttpLoggerForDjango  # noqa
+from ._flask import HttpLoggerForFlask
 from ._requests import ResurfaceLoggerMiddleware  # noqa
 from .requests_adapter import MiddlewareHTTPAdapter  # noqa
 
-__all__ = ["HttpLoggerForDjango", "ResurfaceLoggerMiddleware", "MiddlewareHTTPAdapter"]
+__all__ = [
+    "HttpLoggerForDjango",
+    "ResurfaceLoggerMiddleware",
+    "MiddlewareHTTPAdapter",
+    "HttpLoggerForFlask",
+]
