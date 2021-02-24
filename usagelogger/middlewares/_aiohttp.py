a36e5b4ed931bc7fc82e4482ea46b52f355f1218
Anyesh <sthacruz@gmail.com>
2021-02-24 10:47:22 +0545

    Add aiohttp middleware

---
+++ usagelogger/middlewares/_aiohttp.py
@@ -0,0 +1,24 @@
+import time
+
+from aiohttp import web
+
+from usagelogger import HttpLogger, HttpMessage
+
+
+def HttpLoggerForAIOHTTP(url, rules):
+    logger = HttpLogger(url=url, rules=rules)
+
+    @web.middleware
+    async def resurface_logger_middleware(request, handler):
+        start_time = time.time()
+        response = await handler(request)
+        interval = str((time.time() - start_time) * 1000)
+        print(request.__class__.__name__)
+
+        HttpMessage.send(
+            logger, request=request, response=response, interval=interval,
+        )
+
+        return response
+
+    return resurface_logger_middleware
