27d221808ffa8f0f4ee7080ce4323ad0f28d8db0
Anyesh <sthacruz@gmail.com>
2021-02-28 00:27:08 +0545

    fix: Remove old adapter

--- usagelogger/middlewares/__init__.py
+++ usagelogger/middlewares/__init__.py
@@ -2,11 +2,9 @@ from ._aiohttp import HttpLoggerForAIOHTTP  # noqa
 from ._django import HttpLoggerForDjango  # noqa
 from ._flask import HttpLoggerForFlask  # noqa
 from ._requests import Session  # noqa
-from .requests_adapter import MiddlewareHTTPAdapter  # noqa
 
 __all__ = [
     "HttpLoggerForDjango",
-    "MiddlewareHTTPAdapter",
     "HttpLoggerForFlask",
     "HttpLoggerForAIOHTTP",
     "Session",
