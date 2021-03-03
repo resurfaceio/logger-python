fd3528864dd62a764c5a09f20857277f2008b432
Anyesh <sthacruz@gmail.com>
2021-03-03 10:07:01 +0545

    Add django logger import to run current version without breaking

--- usagelogger/__init__.py
+++ usagelogger/__init__.py
@@ -1,12 +1,16 @@
-# don't import django logger here! (optional, not required)
 from .base_logger import BaseLogger  # noqa
 from .http_logger import HttpLogger  # noqa
 from .http_message import HttpMessage  # noqa
 from .http_request_impl import HttpRequestImpl  # noqa
 from .http_response_impl import HttpResponseImpl  # noqa
 from .http_rules import HttpRules  # noqa
-from .middlewares import _requests as resurface  # noqa
+from .middlewares import requests as resurface  # noqa
 from .usage_loggers import UsageLoggers  # noqa
+
+try:  # To run without breaking the current version
+    from .middlewares import django  # noqa
+except ModuleNotFoundError:
+    pass
 
 __version__ = "2.1.1"
 
@@ -19,4 +23,5 @@ __all___ = [
     "HttpLogger",
     "HttpMessage",
     "resurface",
+    "django",
 ]
