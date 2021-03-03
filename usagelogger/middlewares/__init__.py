17770854b7926f19a22a6fb35950e13270d2583e
Anyesh <sthacruz@gmail.com>
2021-03-03 10:08:37 +0545

    fix: Add try catch for modules

--- usagelogger/middlewares/__init__.py
+++ usagelogger/middlewares/__init__.py
@@ -1,11 +1,18 @@
 try:
-    from ._aiohttp import HttpLoggerForAIOHTTP  # noqa
-    from ._django import HttpLoggerForDjango  # noqa
-    from ._flask import HttpLoggerForFlask  # noqa
-    from ._requests import Session  # noqa
+    from .aiohttp import HttpLoggerForAIOHTTP  # noqa
 except ModuleNotFoundError:
-    from ._requests import Session  # noqa
+    pass
+try:
+    from .django import HttpLoggerForDjango  # noqa
+except ModuleNotFoundError:
+    pass
+try:
+    from .flask import HttpLoggerForFlask  # noqa
+except ModuleNotFoundError:
+    pass
 
+
+from .requests import Session  # noqa
 
 __all__ = [
     "HttpLoggerForDjango",
