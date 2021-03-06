f3328458bb90384d829c316ac66990dcad117c12
Anyesh <sthacruz@gmail.com>
2021-03-06 21:26:46 +0545

    fix: Update aiohttp req and res type

--- usagelogger/aiohttp.py
+++ usagelogger/aiohttp.py
@@ -3,6 +3,8 @@ import time
 from aiohttp import web
 
 from usagelogger import HttpLogger, HttpMessage
+from usagelogger.http_request_impl import HttpRequestImpl
+from usagelogger.http_response_impl import HttpResponseImpl
 
 
 def HttpLoggerForAIOHTTP(url, rules):
@@ -12,12 +14,22 @@ def HttpLoggerForAIOHTTP(url, rules):
     async def resurface_logger_middleware(request, handler):
         start_time = time.time()
         response = await handler(request)
+
         interval = str((time.time() - start_time) * 1000)
 
         HttpMessage.send(
             logger,
-            request=request,
-            response=response,
+            request=HttpRequestImpl(
+                url=str(request.url),
+                headers=request.headers,
+                params=request.query,
+                method=request.method,
+            ),
+            response=HttpResponseImpl(
+                status=response.status,
+                headers=response.headers,
+                body=response.body.decode("utf8"),
+            ),
             interval=interval,
         )
 
