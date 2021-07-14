d06ece1f4373754d87dbc35fc74d76f17b4112a9
Rob Dickinson <robfromboulder@gmail.com>
2021-07-14 15:33:13 -0600

    Update version to 2.2.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
 
 setup(
     name="usagelogger",
-    version="2.2.5",
+    version="2.2.6",
     description="Logging usage of Python-based services, with user privacy by design.",
     long_description=read_file("DESCRIPTIONS.md"),
     long_description_content_type="text/markdown",
