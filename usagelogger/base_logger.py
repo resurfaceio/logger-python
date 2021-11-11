6df43768495a881ca0c93f239e268612c006e357
Anyesh <sthacruz@gmail.com>
2021-11-11 19:28:58 +0545

    fix: Join daemon thread in the end

--- usagelogger/base_logger.py
+++ usagelogger/base_logger.py
@@ -173,6 +173,7 @@ class BaseLogger:
                 worker.name = "submission_thread"
                 worker.setDaemon(True)
                 worker.start()
+                worker.join()  # Not a good practice but required for that success and failure counts
 
             # enclosure_queue.join()  # We don't have to block our main thread.
 
